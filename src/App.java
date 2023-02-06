e0b24e6e4a9efb1ad84b0fb8acd5551f2b5a24ca
modengann <99417989+modengann@users.noreply.github.com>
2023-02-06 09:59:09 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,20 @@
+/**
+ * Overall task: Take three numbers from the user and print the greatest number.
+ * 
+ * Include:
+ * Messages to the user, asking them to enter each number.
+ * Some output to show the result. For example: "The greatest number was ...", where
+ * the ... are replaced by the result
+
+ */
+
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in);
+
+        
+    }
+}
